51b9a4e7d83cdd2fb65f315a9e7ab0dab8f2fb84
pc18 <pc18@pc18-PC>
2019-02-14 09:58:33 +0800

    second commit

---
+++ src/soalan2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pc 19
+ */
+public class soalan2 {
+public static void main (String[] args){
+    String lokasi ="Kota Bahru"
+            double suhu=28.6,kelembapan=78.6;
+            boolean status =true;
+       System.out.println("Laporan Cuaca Hari Ini");
+        System.out.println("Lokasi="+lokasi");
+        System.out.println("suhu="+suhu + (char)186+"C");
+        System.out.println("Kelembapan ="+kelembapan");
+        System.out.println("Amaran Banjir :"+status");
+
+
+
+}    
+}
